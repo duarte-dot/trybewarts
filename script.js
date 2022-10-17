06b9ef99298714d863d73ecb6727e14308f5cc15
saulomagnus <saulomagnus@outlook.com>
2022-10-17 12:31:48 -0300

    Resolve alguns problemas do Lint; retira do HTML os elementos de input da avaliação e coloca no JavaScript

--- script.js
+++ script.js
@@ -24,6 +24,17 @@ agreement.addEventListener('change', () => {
   }
 });
 
+const avaliacao = document.getElementById('avaliacao');
+
+for (let index = 1; index <= 10; index += 1){
+  const novaRadio = document.createElement('input');
+  novaRadio.type = 'radio';
+  novaRadio.name = 'rate';
+  novaRadio.value = index;
+  novaRadio.innerHTML = index;
+  avaliacao.appendChild(novaRadio);
+}
+
 // Altera o contador de caracteres, resolvendo o requisito 20
 const caixaComentario = document.getElementById('textarea');
 const contador = document.getElementById('counter');
