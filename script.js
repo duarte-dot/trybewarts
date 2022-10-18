6d84c29ae8d00abaafa4e1f66a4143015a316119
duarte <gabrieldvr@outlook.com>
2022-10-17 23:01:17 -0300

    requisito 21

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ for (let index = 1; index <= 10; index += 1) {
   novaRadio.type = 'radio';
   novaRadio.name = 'rate';
   novaRadio.value = index;
+  novaRadio.className = 'avaliation';
   novaRadio.innerHTML = index;
   avaliacao.appendChild(novaRadio);
 }
@@ -45,4 +46,67 @@ caixaComentario.addEventListener('input', (param) => {
   const restante = 500 - currentLenght;
 
   contador.innerHTML = restante;
+});
+
+let familyInfo = '';
+function functionFamily(event) {
+  event.preventDefault();
+  document.getElementById('evaluation-form').style.display = 'none';
+
+  for (let i = 0; i < 3; i += 1) {
+    if (document.getElementsByClassName('familia')[i].checked === true) {
+      familyInfo += document
+        .getElementsByClassName('familia')[i].value;
+    }
+  }
+}
+
+let contentInfoString = '';
+const contentInfo = [];
+function functionContent(event) {
+  event.preventDefault();
+  for (let i = 0; i < 6; i += 1) {
+    if (document.getElementsByClassName('subject')[i].checked === true) {
+      contentInfo.push(document
+        .getElementsByClassName('subject')[i].value);
+    }
+  }
+  for (let i = 0; i < contentInfo.length - 1; i += 1) {
+    contentInfoString += `${contentInfo[i]}, `;
+  }
+
+  contentInfoString += contentInfo[contentInfo.length - 1];
+}
+
+let avaliacaoInfo = '';
+function functionAvaliacao(event) {
+  event.preventDefault();
+  for (let i = 0; i < 10; i += 1) {
+    if (document.getElementsByClassName('avaliation')[i].checked === true) {
+      avaliacaoInfo = document.getElementsByClassName('avaliation')[i].value;
+    }
+  }
+}
+const finalInfo = document.getElementById('final-info');
+const formulario = document.createElement('form');
+
+buttonSubmitAll.addEventListener('click', (event) => {
+  document.getElementById('evaluation-form').style.display = 'none';
+  functionContent(event);
+  functionFamily(event);
+  functionAvaliacao(event);
+  finalInfo.appendChild(formulario);
+  formulario.id = 'form-data';
+  const nomeInfo = document.getElementById('input-name').value;
+  const houseInfo = document.getElementById('house').value;
+  const sobrenomeInfo = document.getElementById('input-lastname').value;
+  const emailInfo = document.getElementById('input-email').value;
+  const comentInfo = document.getElementById('textarea').value;
+  formulario.innerText = `Nome: ${nomeInfo} ${sobrenomeInfo}
+  Email: ${emailInfo}
+  Casa: ${houseInfo}
+  Família: ${familyInfo}
+  Matérias: ${contentInfoString}
+  Avaliação: ${avaliacaoInfo}
+  Observações: ${comentInfo}`;
 });
